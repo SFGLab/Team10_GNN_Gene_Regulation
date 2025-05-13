5331915f5957275b87298da8b3afcecd29838501
Kaczyniec <kaczynska.pm@gmail.com>
2025-05-13 19:39:33 +0200

    Preprocessing BEELine, without TFs

---
+++ preprocessing_beeline.py
@@ -0,0 +1,78 @@
+from torch_geometric.data import Data
+from torch_geometric.utils import convert
+import torch
+import pandas as pd
+import os
+import itertools
+import numpy as np
+
+import networkx as nx
+import random
+from collections import Counter
+from torch_geometric.utils import to_networkx
+
+#Edge creation fucnction for the graph - reformatting the gene expression input
+def convert_to_networkx(graph, n_sample=None):
+    g = to_networkx(graph, node_attrs=["x"])
+    y = graph.y.numpy()
+    if n_sample is not None:
+        sampled_nodes = random.sample(g.nodes, n_sample)
+        g = g.subgraph(sampled_nodes)
+        y = y[sampled_nodes]
+    return g, y
+
+
+#Data preprocessing to graph
+def data_preprocessing(folder_name,file_expression,file_ordering,path_gold,final_out):
+
+    hetero = pd.read_csv(os.path.join(folder_name, file_expression), index_col=0)
+    null = pd.read_csv(os.path.join(folder_name, file_ordering), index_col=0)
+    #traject = pd.read_csv(default_path+file_traject,sep='\t')
+    gold = pd.read_csv(path_gold, index_col=False)
+
+    node_features = hetero.join(null)
+    gene_to_index = {row['index']: i for i, row in node_features.reset_index().iterrows()}
+    print("node_features")
+    print(node_features.shape)
+    print(gene_to_index)
+    gold['Gene1'] = gold['Gene1'].apply(lambda x: gene_to_index[x] if x in gene_to_index.keys() else None).dropna()
+    gold['Gene2'] = gold['Gene2'].apply(lambda x: gene_to_index[x] if x in gene_to_index.keys() else None).dropna()
+    basic_data = Data()
+    basic_aug_data = Data()
+
+    basic_data.x = torch.tensor(node_features.values).T
+    #basic_data.edge_attr=edscore
+    basic_data.edge_index = torch.tensor(gold.values).T #edge_lab_index
+
+    #basic_TS_data.x =torch.tensor(listfinal).T
+    #basic_TS_data.edge_attr=edscore
+    #basic_TS_data.edge_index = gold.values #edge_lab_index
+
+    G = convert.to_networkx(basic_data, to_undirected=True)
+    pagerank = nx.algorithms.link_analysis.pagerank_alg.pagerank(G)
+    clustering_coef = nx.algorithms.cluster.clustering(G)
+    betweeness_centrality = nx.betweenness_centrality(G, k=50)
+    degree = G.degree()
+
+    pg_list =list(pagerank.values())
+    clu_coef_list =list(clustering_coef.values())
+    bet_cen_list =list(betweeness_centrality.values())
+
+    deg_list =[x[1] for x in degree]
+
+    basic_aug_data.x = torch.cat([torch.tensor(node_features.values), torch.tensor([pg_list,clu_coef_list,bet_cen_list,deg_list])]).T
+    #basic_aug_data.edge_attr=edscore
+    basic_aug_data.edge_index = torch.tensor(gold.values).T
+
+    return basic_data,basic_aug_data
+
+#Path for the files
+path = "./BEELINE/"
+gold_std = "./BEELINE/hESC-ChIP-seq-network.csv"
+
+final_out = pd.DataFrame()
+
+
+basic_data,basic_aug_data = data_preprocessing(path, "ExpressionData.csv", "GeneOrdering.csv", gold_std, final_out)
+torch. save(basic_data, path+"basic_data_hESC.pt")
+torch. save(basic_aug_data, path+"basic_data_aug_hESC.pt")
