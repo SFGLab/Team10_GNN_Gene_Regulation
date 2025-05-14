32983d8dc3e072b93eb3f1e1bb6cbb413e85b2f2
Jacob Bumgarner <jacob.bumgarner@bms.com>
2025-05-14 12:57:03 -0400

    handle aliased symbols; add base pair bin

--- symbol_mapping/map_hgnc_to_chr.R
+++ symbol_mapping/map_hgnc_to_chr.R
@@ -14,10 +14,10 @@
 # - Slim Mapped Symbols: A list of the first occurring transcript (lowest
 #   chromosomal start position) for each mapped input gene.
 
-
 #### SCRIPT INPUTS ####
 INPUT_SYMBOLS_FILE <- "example_input.csv"
 OUTPUT_FILE <- "example_output.csv"
+BIN_BP_RESOLUTION <- 5000
 #### END SCRIPT INPUTS ####
 
 #### Outline ####
@@ -66,7 +66,7 @@ save_mapped_symbols <- function(mapped_symbols, out_file) {
 
   # Slim down the mapped symbols
   mapped_symbols_slim <- mapped_symbols |>
-    dplyr::group_by(Symbol) |>
+    dplyr::group_by(Symbol, Symbol.Alt) |>
     dplyr::slice_min(Start.BP, n = 1, with_ties = FALSE)
 
   base_out_file <- basename(out_file)
@@ -77,30 +77,51 @@ save_mapped_symbols <- function(mapped_symbols, out_file) {
 }
 
 #' Map an input gene to the hg38 reference genome.
-map_symbols <- function(symbols) {
+map_symbols <- function(input_symbols, bp_bin_width = 5000) {
   # Prepare DBs
   library(org.Hs.eg.db)
   library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 
   symbols <- AnnotationDbi::select(
     org.Hs.eg.db,
-    keys = symbols$Symbol,
+    keys = input_symbols$Symbol,
     columns = c("SYMBOL", "ENSEMBL", "ENTREZID"),
     keytype = "SYMBOL"
-  )
+  ) |>
+    tibble::as_tibble()
 
   # Warn about unmapped symbols.
   not_mapped <- symbols |>
-    dplyr::filter(is.na(ENTREZID)) |>
-    dplyr::pull(SYMBOL)
-  if (length(not_mapped)) {
-    cli::cli_warn(
-      c(
-        "Some of the input gene symbols couldn't be mapped to the {.field org.Hs.eg.db} annotation data base.",
-        "i" = "The symbols that failed to map will be in the output file with `NA` chromosome columns.",
-        "i" = "The symbols that failed to map will not be included in the slim output file."
+    dplyr::filter(is.na(ENTREZID))
+  symbols <- symbols |>
+    dplyr::filter(!is.na(ENTREZID))
+
+  if (nrow(not_mapped)) {
+    cli::cli_alert("Attempting to remap {nrow(not_mapped)} symbols using aliases.")
+
+    remapped <- AnnotationDbi::select(
+      org.Hs.eg.db,
+      not_mapped$SYMBOL,
+      columns = c("SYMBOL", "ALIAS", "ENSEMBL", "ENTREZID"),
+      keytype = "ALIAS"
+    ) |>
+      tibble::as_tibble() |>
+      dplyr::rename(SYMBOL = ALIAS, SYMBOL.ALT = SYMBOL)
+
+    final_unmapped <- any(is.na(remapped$ENTREZID))
+
+    symbols <- dplyr::bind_rows(symbols, remapped) |>
+      dplyr::arrange(SYMBOL)
+
+    if (final_unmapped) {
+      cli::cli_warn(
+        c(
+          "Some of the input gene symbols couldn't be mapped to the {.field org.Hs.eg.db} annotation data base.",
+          "i" = "The symbols that failed to map will be in the output file with `NA` chromosome columns.",
+          "i" = "The symbols that failed to map will not be included in the slim output file."
+        )
       )
-    )
+    }
   }
 
   mapped <- AnnotationDbi::select(
@@ -111,11 +132,14 @@ map_symbols <- function(symbols) {
   )
 
   mapped <- mapped |>
-    dplyr::left_join(symbols, by = c("GENEID" = "ENTREZID")) |>
+    dplyr::left_join(symbols,
+                     by = c("GENEID" = "ENTREZID"),
+                     relationship = "many-to-many") |>
     tibble::as_tibble()
 
   new_colnames <- c(
     "SYMBOL" = "Symbol",
+    "SYMBOL.ALT" = "Symbol.Alt",
     "ENSEMBL" = "Ensembl.ID",
     "GENEID" = "Entrez.ID",
     "TXCHROM" = "Chromosome",
@@ -126,6 +150,15 @@ map_symbols <- function(symbols) {
 
   colnames(mapped) <- new_colnames[colnames(mapped)]
 
+  # filter out alt chr info
+  mapped <- mapped |>
+    dplyr::filter(grepl("^chr[0-9XYM]+$", Chromosome)) |>
+    dplyr::arrange(Symbol)
+
+  # Add bin resolution
+  mapped <- mapped |>
+    dplyr::mutate(Bin.ID = Start.BP %/% bp_bin_width)
+
   mapped
 }
 
@@ -133,7 +166,7 @@ map_symbols <- function(symbols) {
 
 #### Run Script ####
 input_symbols <- load_user_symbols(input_symbols_file = INPUT_SYMBOLS_FILE)
-mapped_symbols <- map_symbols(symbols = input_symbols)
+mapped_symbols <- map_symbols(input_symbols = input_symbols, bp_bin_width = BIN_BP_RESOLUTION)
 save_mapped_symbols(mapped_symbols = mapped_symbols, out_file = OUTPUT_FILE)
 
 #### End Run Script ####
